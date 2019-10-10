d930e00d36e7af7f2f299182b9418f657d1e02f2
Josema <enzo@josema.es>
2019-10-10 16:56:13 +0200

    Removed panLimitsOrbitControls

--- testbrowser/orbitcontrols/script.js
+++ testbrowser/orbitcontrols/script.js
@@ -1,8 +1,8 @@
 const RAD2DEG = 180 / Math.PI
 const DEG2RAD = Math.PI / 180
 
-const distance = 10
-const fov = 50
+const distance = 100
+const fov = 10
 const near = 1
 const far = 999999
 
@@ -24,19 +24,87 @@ const controls = new THREE.OrbitControls(camera, renderer.domElement)
 controls.mouseButtons = { LEFT: THREE.MOUSE.PAN, RIGHT: THREE.MOUSE.ROTATE }
 controls.touches = { ONE: THREE.TOUCH.PAN, TWO: THREE.TOUCH.DOLLY_PAN }
 // controls.enableDamping = true // if true has a bug when zooming on limits
-controls.dampingFactor = 0.1
+// controls.dampingFactor = 0.1
 // controls.screenSpacePanning = true // this is what three-iso-game-camera actually does
-controls.minDistance = 10
-controls.maxDistance = 50
+// controls.minDistance = 10
+// controls.maxDistance = 50
 // controls.maxPolarAngle = Math.PI / 2
 
-camera.position.set(5, 5, 5)
-controls.update()
+const maxX = 25
+const minX = -Infinity
+const maxZ = 25
+const minZ = -Infinity
+let angleV = 30
+let angleH = 45
+let positionX
+let positionZ
+
+changePolarToCartesian({ angleV, angleH, distance })
+controls.addEventListener('change', e => {
+    const tempV = Math.round(controls.getPolarAngle() * RAD2DEG)
+    const tempH = Math.round(controls.getAzimuthalAngle() * RAD2DEG)
+    const x = controls.target.x
+    if (x < minX || x > maxX) {
+        controls.target.setX(x < minX ? minX : maxX)
+        camera.position.setX(positionX)
+        changePolarToCartesian({ angleV, angleH })
+    }
+
+    // if (x < minX || x > maxX) {
+    //     controls.target.setX(x < minX ? minX : maxX)
+    //     camera.position.setX(positionX)
+    //     changePolarToCartesian({ angleV, angleH })
+    // }
+
+    // Updating state
+    positionX = camera.position.x
+    positionY = camera.position.y
+    angleV = tempV
+    angleH = tempH
+})
+
+function targetTo(x, z, controls, camera) {
+    const diffX = controls.target.x - x
+    const diffZ = controls.target.z - z
+    controls.target.setX(x)
+    controls.target.setZ(z)
+    camera.position.setX(camera.position.x - diffX)
+    camera.position.setZ(camera.position.z - diffZ)
+}
+
+function changePolarToCartesian({
+    angleV,
+    angleH,
+    distance = camera.position.distanceTo(controls.target)
+}) {
+    const point = polarToCartesian(angleV, angleH, distance)
+    camera.position.set(
+        point.x + controls.target.x,
+        point.y + controls.target.y,
+        point.z + controls.target.z
+    )
+    controls.update()
+}
+
+//  https://gist.github.com/jhermsmeier/72626d5fd79c5875248fd2c1e8162489
+function polarToCartesian(angleV, angleH, radius) {
+    const phi = angleV * DEG2RAD
+    const theta = angleH * DEG2RAD
+    return {
+        x: radius * Math.sin(phi) * Math.sin(theta),
+        y: radius * Math.cos(phi),
+        z: radius * Math.sin(phi) * Math.cos(theta)
+    }
+}
 
 // .... setup and run demo-code
-const panLimits = panLimitsOrbitControls({ camera, controls })
 function animate(time) {
-    panLimits.update({ maxX: 25, maxZ: 25 })
+    document.getElementById('output').innerHTML = `${Math.round(
+        controls.getPolarAngle() * RAD2DEG
+    )} ${Math.round(controls.getAzimuthalAngle() * RAD2DEG)} ${Math.round(
+        camera.position.distanceTo(controls.target)
+    )}`
+
     renderer.render(scene, camera)
     requestAnimationFrame(animate)
 }
@@ -50,75 +118,6 @@ window.addEventListener('resize', ev => {
 })
 
 document.body.appendChild(renderer.domElement)
-
-function targetTo(x, z, controls, camera) {
-    const diffX = controls.target.x - x
-    const diffZ = controls.target.z - z
-    controls.target.setX(x)
-    controls.target.setZ(z)
-    camera.position.setX(camera.position.x - diffX)
-    camera.position.setZ(camera.position.z - diffZ)
-    controls.update()
-}
-
-function changePolarToCartesian(angleV, angleH, distance) {
-    distance = distance || camera.position.distanceTo(controls.target)
-    const point = polarToCartesian(angleV, angleH, distance)
-    camera.position.set(
-        point.x + controls.target.x,
-        point.y + controls.target.y,
-        point.z + controls.target.z
-    )
-    // camera.lookAt(new THREE.Vector3(0, 0, 0))
-    controls.update()
-}
-
-function panLimitsOrbitControls({ camera, controls }) {
-    let positionX
-    let positionZ
-    return {
-        update: ({
-            maxX = Infinity,
-            minX = -Infinity,
-            maxZ = Infinity,
-            minZ = -Infinity
-        }) => {
-            // console.log(controls.getPolarAngle())
-            const targetX = controls.target.x
-            if (minX <= targetX && maxX >= targetX) {
-                positionX = camera.position.x
-            } else if (targetX > maxX) {
-                camera.position.setX(positionX)
-                controls.target.setX(maxX)
-            } else if (targetX < minX) {
-                camera.position.setX(positionX)
-                controls.target.setX(minX)
-            }
-
-            const targetZ = controls.target.z
-            if (minZ <= targetZ && maxZ >= targetZ) {
-                positionZ = camera.position.z
-            } else if (targetZ > maxZ) {
-                camera.position.setZ(positionZ)
-                controls.target.setZ(maxZ)
-            } else if (targetZ < minZ) {
-                camera.position.setZ(positionZ)
-                controls.target.setZ(minZ)
-            }
-        }
-    }
-}
-
-//  https://gist.github.com/jhermsmeier/72626d5fd79c5875248fd2c1e8162489
-function polarToCartesian(angleV, angleH, radius) {
-    var phi = (90 - angleV) * DEG2RAD
-    var theta = (angleH + 180) * DEG2RAD
-    return {
-        x: -(radius * Math.sin(phi) * Math.sin(theta)),
-        y: radius * Math.cos(phi),
-        z: -radius * Math.sin(phi) * Math.cos(theta)
-    }
-}
 
 // geometry
 scene.add(new THREE.GridHelper(50, 100, 0xaaaaaa, 0x999999))
@@ -136,3 +135,30 @@ scene.add(
 const dirLight = new THREE.DirectionalLight()
 dirLight.position.set(1, 0.4, 0.2)
 scene.add(dirLight, new THREE.AmbientLight(0x444444))
+
+// const panLimits = panLimitsOrbitControls({ camera, controls })
+// function panLimitsOrbitControls({ camera, controls }) {
+//     let positionX
+//     let positionY
+//     let positionZ
+//     let targetX
+//     let targetY
+//     let targetZ
+
+//     return {
+//         update: ({
+//             maxX = Infinity,
+//             minX = -Infinity,
+//             maxZ = Infinity,
+//             minZ = -Infinity
+//         }) => {
+//             if (minX <= controls.target.x && maxX >= controls.target.x) {
+//                 positionX = camera.position.x
+//             } else if (controls.target.x > maxX) {
+//                 // camera.position.setX(positionX)
+//                 controls.target.setX(maxX)
+//                 changePolarToCartesian(angleV, angleH)
+//             }
+//         }
+//     }
+// }
